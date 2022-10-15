a33e9669ea63567c99b77223c026ed00b3a9d072
roi <roro8823100@gmail.com>
2022-10-16 00:13:00 +0300

    commit

--- app/src/main/java/com/example/wordle/GameActivity.java
+++ app/src/main/java/com/example/wordle/GameActivity.java
@@ -4,94 +4,104 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView[][] textViewArray = new TextView[5][6];
+    TextView[][] textViewArray = new TextView[4][5];
+    LinearLayout llMain;
     EditText et_words;
-    TextView tv_answer;
     String word;
     SharedPreferences sp;
     Button btn_submit;
     Random rand = new Random();
-    int row =1;
+    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+    int row = 0;
+    boolean didntWin = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        btn_submit= findViewById(R.id.btn_submit);
+        llMain = findViewById(R.id.llMain);
+        lp.setMargins(20, 20, 20, 20);
+        btn_submit = findViewById(R.id.btn_submit);
         btn_submit.setOnClickListener(this);
 
-        textViewArray[1][1] = findViewById(R.id.tv1_1);
-        textViewArray[1][2] = findViewById(R.id.tv1_2);
-        textViewArray[1][3] = findViewById(R.id.tv1_3);
-        textViewArray[1][4] = findViewById(R.id.tv1_4);
-        textViewArray[1][5] = findViewById(R.id.tv1_5);
+        for (int i = 0; i < textViewArray.length; i++) {
+            LinearLayout llTemp = new LinearLayout(this);
+            llTemp.setLayoutParams(lp);
+            llTemp.setGravity(Gravity.CENTER_HORIZONTAL);
+            llMain.addView(llTemp);
+            for (int k = 0; k < textViewArray[i].length; k++) {
+                textViewArray[i][k] = new TextView(this);
+                textViewArray[i][k].setLayoutParams(lp);
+                llTemp.addView(textViewArray[i][k]);
+                textViewArray[i][k].setText("_");
 
-        textViewArray[2][1] = findViewById(R.id.tv2_1);
-        textViewArray[2][2] = findViewById(R.id.tv2_2);
-        textViewArray[2][3] = findViewById(R.id.tv2_3);
-        textViewArray[2][4] = findViewById(R.id.tv2_4);
-        textViewArray[2][5] = findViewById(R.id.tv2_5);
-
-        textViewArray[3][1] = findViewById(R.id.tv3_1);
-        textViewArray[3][2] = findViewById(R.id.tv3_2);
-        textViewArray[3][3] = findViewById(R.id.tv3_3);
-        textViewArray[3][4] = findViewById(R.id.tv3_4);
-        textViewArray[3][5] = findViewById(R.id.tv3_5);
-
-        textViewArray[4][1] = findViewById(R.id.tv4_1);
-        textViewArray[4][2] = findViewById(R.id.tv4_2);
-        textViewArray[4][3] = findViewById(R.id.tv4_3);
-        textViewArray[4][4] = findViewById(R.id.tv4_4);
-        textViewArray[4][5] = findViewById(R.id.tv4_5);
-
+            }
+        }
         et_words = findViewById(R.id.et_words);
-        tv_answer = findViewById(R.id.tv_answer);
         sp = getSharedPreferences("words", MODE_PRIVATE);
-        int number_of_words=sp.getInt("number_of_words",0);
-        int random = rand.nextInt(number_of_words+1);
-        word =sp.getString((random+""),"nadav");
-
-        tv_answer.setText(word);
+        int number_of_words = sp.getInt("number_of_words", 0);
+        int random = rand.nextInt(number_of_words + 1);
+        word = sp.getString((random + ""), "nadav");
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view==btn_submit&&row<5){
+        if (view == btn_submit && row < textViewArray.length && didntWin) {
             String guess = et_words.getText().toString();
-            if(guess.length()==5){
-                for (int i = 1; i <=5 ; i++) {
+            if (guess.length() == 5) {
+                for (int i = 0; i <= 4; i++) {
                     textViewArray[row][i].setTextSize(25);
-                    textViewArray[row][i].setText(""+guess.charAt(i-1));
-                    if(word.indexOf(guess.charAt(i-1))>-1){
+                    textViewArray[row][i].setText("" + guess.charAt(i));
+                    if (word.indexOf(guess.charAt(i)) > -1) {
                         textViewArray[row][i].setBackgroundColor(Color.YELLOW);
                     }
-                    if ( (""+guess.charAt(i-1)).equals(word.charAt(i-1)+"")){
+                    if (("" + guess.charAt(i)).equals(word.charAt(i) + "")) {
                         textViewArray[row][i].setBackgroundColor(Color.GREEN);
                     }
                     et_words.setText("");
 
 
                 }
-                if (word.equals(guess)){
-                    tv_answer.setText("u wonnnnnnnnnnnn");
+                if (word.equals(guess) || row == textViewArray.length-1) {
+                    didntWin = false;
+                    TextView win = new TextView(this);
+                    win.setLayoutParams(lp);
+                    llMain.addView(win);
+                    win.setTextColor(Color.BLUE);
+                    win.setTextSize(50);
+
+                    if (word.equals(guess)) {
+                        win.setText("you won");
+                    }
+                    else {
+                        win.setText("you lost");
+                    }
+
 
                 }
-                row+=1;
+                row += 1;
 
             }
 
 
         }
+
+
     }
 }
+
